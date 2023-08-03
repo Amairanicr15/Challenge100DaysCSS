3b719386d5a68bbe56633e6d047bc4d4a47e5507
Amairani Carrillo <cramairani5@gmail.com>
2023-08-03 13:36:53 -0600

    Falto agregar js al reto 7

--- Day #7/main.js
+++ Day #7/main.js
@@ -1,4 +1,11 @@
-/*
-
-
- */
+document.querySelectorAll('.search-icon').forEach(function(element){
+    element.addEventListener('click', function(){
+        document.querySelector('.search-input').classList.toggle('active');
+    });
+});
+document.querySelectorAll('.menu-icon').forEach(function(element){
+    element.addEventListener('click', function(){
+        document.querySelector('.panel').classList.toggle('show-menu');
+        document.querySelector('.menu').classList.toggle('active');
+    });
+});
